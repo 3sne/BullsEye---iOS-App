4a4318d17df281779dc973ff7d721347551bb436
Mukur Panchani <panchanimukur@gmail.com>
2018-09-05 12:29:08 +0530

    First Commit: Alert Working

---
+++ BullsEye/BullsEye/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by iOS Lab on 05/09/18.
+//  Copyright © 2018 Mukur Panchani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello World", message: "What Up?", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Cool", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+}
+
