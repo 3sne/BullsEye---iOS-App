a7110e3f55d52e3f7010f60a2f63dae5f5c4d81e
3sne <panchanimukur@gmail.com>
2018-09-09 18:51:17 +0530

    Add About and AboutMe Views.C: vid 36

---
+++ BullsEye/BullsEye/AboutAuthorViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AboutAuthorViewController.swift
+//  BullsEye
+//
+//  Created by ios lab on 09/09/18.
+//  Copyright © 2018 Mukur Panchani. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by ios lab on 09/09/18.
+//  Copyright © 2018 Mukur Panchani. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
